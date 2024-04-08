1be1759baa1a41bacf2d7482bff0702ca11b683d
Bennett Moore <bennettmoore05@gmail.com>
2024-04-08 12:03:05 -0400

    add context property and comments

--- src/services/Theme.hpp
+++ src/services/Theme.hpp
@@ -8,15 +8,19 @@ class Theme : public QObject
 {
     Q_OBJECT
 public:
+    // Get the Theme singleton instance
     static Theme *instance();
 
-    Q_INVOKABLE QString color(QString key, double opacity);
+    // Get an ARGB hex color code from a theme key and opacity
+    Q_INVOKABLE QString color(QString key, double opacity = 1);
 
 private:
     explicit Theme(QObject *parent = nullptr);
 
+    // Returns an ARGB hex color code given HSV values
     QString hsv(int hue, double sat, double val);
 
+    // Saves all color keys and values
     QMap<QString, QString> m_colors;
     static Theme *m_instance;
 };

f9ce33c4e968da3a0daa372391001802a6defa16
Bennett Moore <bennettmoore05@gmail.com>
2024-04-22 11:43:14 -0400

    function defs and Password struct

--- src/PasswordEditor.cpp
+++ src/PasswordEditor.cpp
@@ -3,3 +3,18 @@
 PasswordEditor::PasswordEditor(QObject *parent)
     : QObject{parent}
 {}
+
+void PasswordEditor::add(Password passwordObject)
+{
+    Q_UNUSED(passwordObject)
+}
+
+PasswordEditor::Password PasswordEditor::edit(QString title)
+{
+    Q_UNUSED(title)
+}
+
+bool PasswordEditor::remove(QString title)
+{
+    Q_UNUSED(title)
+}
--- src/PasswordEditor.hpp
+++ src/PasswordEditor.hpp
@@ -7,4 +7,15 @@ class PasswordEditor : public QObject
     Q_OBJECT
 public:
     explicit PasswordEditor(QObject *parent = nullptr);
+
+    struct Password
+    {
+        QString title;
+        QString username;
+        QString password;
+    };
+
+    Q_INVOKABLE void add(Password passwordObject);
+    Q_INVOKABLE Password edit(QString title);
+    Q_INVOKABLE bool remove(QString title);
 };
